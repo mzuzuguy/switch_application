1f502997b2403f517cba6255158a8478c282af54
efp-bsc-com-02-22-png <efp-bsc-com-02-22@unima.ac.mw>
2026-05-08 09:15:29 +0100

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TransactionController } from './transaction/transaction.controller';
+import { TransactionModule } from './transaction/transaction.module';
+
+@Module({
+  imports: [TransactionModule],
+  controllers: [AppController, TransactionController],
+  providers: [AppService],
+})
+export class AppModule {}
