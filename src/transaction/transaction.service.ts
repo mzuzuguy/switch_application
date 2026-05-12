11c99f5ae9c9e55f5549f052916053a522bf999e
efp-bsc-com-02-22-png <efp-bsc-com-02-22@unima.ac.mw>
2026-05-12 09:34:19 +0100

    aaded comment

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -4,6 +4,7 @@ import {Repository} from 'typeorm';
 
 @Injectable()
 export class TransactionService {
+    //injects the transaction repository so that we can talk to the database
     constructor(
         @InjectRepository(Transaction)
         private transactionRepository: Repository<Transaction>
